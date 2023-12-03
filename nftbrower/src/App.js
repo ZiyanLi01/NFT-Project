dfd8510abc429426198d4c5c2029d5670c20ac23
Ziyan Li <liziyan1992@gmail.com>
2023-12-03 17:29:10 +0800

    design layout

--- nftbrower/src/App.js
+++ nftbrower/src/App.js
@@ -1,19 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
-import { searchNFTs } from "./utils";
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
 
 function App() {
-  const handleButtonClick = () => {
-    searchNFTs("car");
-  };
-
   return (
-    <div className="App">
-      <Button type="primary" onClick={handleButtonClick}>
-        Test
-      </Button>
-    </div>
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
+          NFT Browser
+        </div>
+      </Header>
+      <Content
+        style={{ height: "calc(100% - 64px)", padding: 20, overflow: "auto" }}
+      ></Content>
+    </Layout>
   );
 }
 
